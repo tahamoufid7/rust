684ca060b5fb3d7deebd002910a5c418e6891806
Taha Moufid <tahamoufid7@gmail.com>
2022-11-15 00:04:21 -0500

    Continued chapter 2 and learned about iterators

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -1,5 +1,24 @@
 use std::io::stdin;
 
+struct Visitor{
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self){
+        println!("{}", self.greeting);
+    }
+}
+
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -11,7 +30,18 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
+    
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("bob", "Wow, who invited Bob?!"),
+    ];
+
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name)
+    let known_visitor = visitor_list.iter().find(| visitor | visitor.name == name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You are not the visitor list. Please leave.")
+    }
 }
