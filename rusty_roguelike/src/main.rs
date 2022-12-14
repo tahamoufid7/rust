21d7286285761882fc1ec4ab64f2147561b8ee61
Taha Moufid <tahamoufid7@gmail.com>
2022-12-13 20:14:34 -0500

    Started using the Legion ECS

--- rusty_roguelike/src/main.rs
+++ rusty_roguelike/src/main.rs
@@ -37,6 +37,11 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         Self {
@@ -55,6 +60,7 @@ impl GameState for State {
         ctx.cls();
         self.resources.insert(ctx.key);
         self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error!");
     }
     
 }
